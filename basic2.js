f307932b6e0b09a3c7aec72cc0dd3e79049211ee
tajtanvvir1 <tanvirah1223@gmail.com>
2024-01-27 23:53:08 +0600

    basic 2 update

---
+++ basic2.js
@@ -0,0 +1,47 @@
+// 1. no keyword in variable name
+var false = 45;
+var var = 50;
+
+// 2. no space or gap in variable name
+
+var is happy = false;
+var inhappy = true;
+
+// 3. no quote
+
+var "address" = "akashe";
+
+// 4. can not start with a number. but  number other than first letter is allowed.
+
+var money = 45;
+var money2 = 55;
+var 3money = 88;
+
+// 5. name is case sensitive
+
+address
+Address
+ADDRESS
+addRess
+ADDress
+
+// 6. how to write a long variable name
+
+var my current home address = "Bandarban Sundorban"; // not accept as one variable
+var mycurrenthomeaddress = "Bandarban Sundorban"; //not readable
+var my_current_home_address = "Bandarban Sundorban"; // snake case
+var myCurrentHomeAddress = "Bandarban Sundorban"; // Camel Case
+var MyCurrentHomeAddress = "Bandarban Sundorban"; //  Pascal Case
+
+console.log(MyCurrentHomeAddress)
+
+
+
+age = 20;
+var money = '30500';
+var yourname = true;
+
+console.log(age)
+
+
+
