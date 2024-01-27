178fa7651ce1d9064fd19c9f4a60832c868a4dbf
tajtanvvir1 <tanvirah1223@gmail.com>
2024-01-27 23:39:58 +0600

    Basic practice

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.log(33)
+
+var age = 17;
+age = 20;
+var money = '30500';
+var yourname = true;
+
+console.log(age)
+console.log(money)
+
+console.log(typeof age)
+console.log(typeof money)
+console.log(yourname)
+console.log(typeof yourname)
