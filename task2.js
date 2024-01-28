ded37395305f062e24f044411ee664056cadcfb7
tajtanvvir1 <tanvirah1223@gmail.com>
2024-01-28 22:46:23 +0600

    task 2 update

---
+++ task2.js
@@ -0,0 +1,15 @@
+// Mathematics, Biology, Chemistry, Physics, and Bangla
+
+var Mathematics = 75 ;
+var Biology = 80 ;
+var Chemistry = 65 ;
+var Physics = 85 ;
+var Bangla = 90 ;
+
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla
+
+// console.log(totalMarks)
+
+var marks = totalMarks / 5 ;
+
+console.log(marks)
