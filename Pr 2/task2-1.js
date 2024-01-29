c0e07d7a6485706a49bf4726984dcc32157bc22d
tajtanvvir1 <tanvirah1223@gmail.com>
2024-01-29 23:24:54 +0600

    17.1 task update

---
+++ Pr 2/task2-1.js
@@ -0,0 +1,16 @@
+/* Free Drinks
+- Burger more than 500tk: free Coke
+- Else Coke: 30tk
+*/
+
+var burger = 600;
+burger = 400;
+const coke = 30;
+
+if (burger > 500) {
+   console.log('coke free')
+}
+
+else {
+   console.log('Coke er 30 taka den')
+}
