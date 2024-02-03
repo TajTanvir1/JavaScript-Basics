c4a3c9d5c68208f2275db41cc98a041934e2abf7
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-03 16:34:26 +0600

    20 test

--- basic.js
+++ basic.js
@@ -1,16 +1,27 @@
-console.log(33)
+// console.log(33)
 
-var age = 17;
-age = 20;
-var money = '30500';
-var yourname = true;
+// var age = 17;
+// age = 20;
+// var money = '30500';
+// var yourname = true;
 
-console.log(age)
-console.log(money)
+// console.log(age)
+// console.log(money)
 
-console.log(typeof age)
-console.log(typeof money)
-console.log(yourname)
-console.log(typeof yourname)
+// console.log(typeof age)
+// console.log(typeof money)
+// console.log(yourname)
+// console.log(typeof yourname)
 
+// function getMenu() {
+//    console.log("Burger, Pizza, Pasta");
+//   }
 
+let ages = [20, 10, 50, 40, 60, 70]
+
+ages.pop(3);
+
+console.log(ages)
+
+  
+  
